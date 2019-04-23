1053e0bc9519ec02d9c0600ee67a476887dfd0b0
bansheng <dyadongcs@gmail.com>
2019-04-23 10:50:29 +0800

    modify res_gru_net.sh

---
+++ experiments/scripts/res_gru_noBN_net.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+
+NET_NAME=ResidualGRUNoBNNet # 修改默认的Net
+EXP_DETAIL=default_model
+OUT_PATH='./output/'$NET_NAME/$EXP_DETAIL
+LOG="$OUT_PATH/log.`date +'%Y-%m-%d_%H-%M-%S'`"
+
+# Make the dir if it not there
+mkdir -p $OUT_PATH
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+export THEANO_FLAGS="floatX=float32,device=cuda0,optimizer_including=cudnn,assert_no_cpu_op='raise'"
+
+python3 main.py \
+      --batch-size 20 \
+      --iter 20000 \
+      --out $OUT_PATH \
+      --model $NET_NAME \
+      ${*:1}
+
+python3 main.py \
+      --test \
+      --batch-size 1 \
+      --out $OUT_PATH \
+      --weights $OUT_PATH/checkpoint.tar \
+      --model $NET_NAME \
+      ${*:1}
