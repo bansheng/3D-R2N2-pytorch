81d77042a2493db7c273ea6023906899aef99ea7
bansheng <1318488477@qq.com>
2019-04-24 16:51:00 +0800

    save time change

--- lib/config.py
+++ lib/config.py
@@ -81,7 +81,7 @@ __C.TRAIN.MOMENTUM = 0.90
 # (for cross entropy loss). Too high regularization will also hinder training.
 __C.TRAIN.WEIGHT_DECAY = 0.00005
 __C.TRAIN.LOSS_LIMIT = 2  # stop training if the loss exceeds the limit
-__C.TRAIN.SAVE_FREQ = 10000  # weights will be overwritten every save_freq
+__C.TRAIN.SAVE_FREQ = 2000  # weights will be overwritten every save_freq
 __C.TRAIN.PRINT_FREQ = 40
 __C.TRAIN.SHOW_LOSS = False
 __C.TRAIN.LOSS_CHANGE = False
