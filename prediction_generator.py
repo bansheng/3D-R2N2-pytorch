60d1e27466e97dd49d7b84f8ee9c2e24be37ce90
bansheng <dyadongcs@gmail.com>
2019-04-25 16:31:50 +0800

    add all dir

--- prediction_generator.py
+++ prediction_generator.py
@@ -1,29 +1,41 @@
 import os
 
 from demo import main, set_pred_file_name
-from lib.config import cfg, cfg_from_list
+from lib.config import cfg_from_list
 
-checkpoint_dir = './output/ResidualGRUNet/default_model'
-pre_dir = './prediction/ResidualGRUNet/'
+Checkpoint_dir = ['./output/ResidualGRUNet/default_model',
+                  './output/GRUNet/default_model',
+                  './output/ResidualGRUNoBNNet/default_model',
+                  './output/ResidualGRUNet_theano/default_model',
+                  './output/ResidualGRUNet_No_Regularition/default_model'
+                  ]
+Pre_dir = ['./prediction/ResidualGRUNet/',
+           './prediction/GRUNet/',
+           './prediction/ResidualGRUNoBNNet/',
+           './prediction/ResidualGRUNet_theano/',
+           './prediction/ResidualGRUNet_No_Regularition/',
+           ]
 
 if __name__ == "__main__":
     # solver_init()
-    for index in range(1, 11):
-        checkpoint_path = os.path.join(checkpoint_dir, 'checkpoint.%d.tar' % (index*2000))
-        # print(save_path)
-        symlink_path = os.path.join(checkpoint_dir, 'checkpoint.tar')
-        if os.path.lexists(symlink_path):
-            os.remove(symlink_path)
-        os.symlink("%s" % os.path.abspath(checkpoint_path), symlink_path)
+    for checkpoint_dir, pre_dir in zip(Checkpoint_dir, Pre_dir):
+        print(checkpoint_dir, pre_dir)
+        for index in range(1, 11):
+            checkpoint_path = os.path.join(checkpoint_dir, 'checkpoint.%d.tar' % (index*2000))
+            # print(save_path)
+            symlink_path = os.path.join(checkpoint_dir, 'checkpoint.tar')
+            if os.path.lexists(symlink_path):
+                os.remove(symlink_path)
+            os.symlink("%s" % os.path.abspath(checkpoint_path), symlink_path)
 
-        # set batch_size = 1
-        cfg_from_list(['CONST.BATCH_SIZE', 1])
+            # set batch_size = 1
+            cfg_from_list(['CONST.BATCH_SIZE', 1])
 
-        # set_pred_file_name
-        name = os.path.join(pre_dir, 'prediction.%d.obj' % (index*2000))
+            # set_pred_file_name
+            name = os.path.join(pre_dir, 'prediction.%d.obj' % (index*2000))
 
-        set_pred_file_name(name)
+            set_pred_file_name(name)
 
-        main()
-        # Make a symlink to the latest network params
-        # os.symlink("%s" % os.path.abspath(checkpoint_path), symlink_path)
+            main()
+            # Make a symlink to the latest network params
+            # os.symlink("%s" % os.path.abspath(checkpoint_path), symlink_path)
