d076109a731c3236352a63c984f179eae7362ac0
bansheng <dyadongcs@gmail.com>
2019-05-19 23:01:03 +0800

    add labersize

--- loss_view.py
+++ loss_view.py
@@ -5,19 +5,21 @@ import numpy as np
 
 Save_dirs = [
     './output/ResidualGRUNet/default_model',
-    # './output/GRUNet/default_model',
+    './output/GRUNet/default_model',
     # './output/ResidualGRUNoBNNet/default_model',
     # './output/ResidualGRUNet_theano/default_model',
-    './output/ResidualGRUNet_No_Regularition/default_model'
+    # './output/ResidualGRUNet_No_Regularition/default_model'
 ]
 
 Labels = [
     'ResidualGRUNet',
-    # 'GRUNet',
+    'GRUNet',
     # 'ResidualGRUNoBNNet',
     # 'ResidualGRUNet_theano',
-    'ResidualGRUNet_No_Regularition'
+    # 'ResidualGRUNet_No_Regularition'
 ]
+
+font = {'family': 'Times New Roman', 'weight': 'normal', 'size': 15}
 
 def file_read(files):
     # files [] 文件名列表
@@ -29,7 +31,7 @@ def file_read(files):
                 ylist = f.readlines()
             for y in ylist:
                 y = y.strip()
-            ylines.append(ylist[:20001:100])
+            ylines.append(ylist[:201:1])
     return ylines
 
 if __name__ == '__main__':
@@ -48,14 +50,15 @@ if __name__ == '__main__':
         plt.plot(axis_x, axis_y.flatten(), color=colors[id], linewidth=1, label=Labels[id])
         print(colors[id])
 
-    plt.legend(loc='upper right')
+    plt.legend(loc='upper right', prop=font)
     plt.xlim((0, xmax))
     plt.ylim((0, 1))
-    plt.xlabel('train iter')
-    plt.ylabel('loss')
+    plt.xlabel('train iter', font)
+    plt.ylabel('loss', font)
     new_ticks = np.linspace(0, xmax, 5)
     plt.xticks(new_ticks)
     plt.yticks([0, 0.5, 1])
+    plt.tick_params(labelsize=14)
     ax = plt.gca()
     ax.spines['right'].set_color('none')
     ax.spines['top'].set_color('none')
